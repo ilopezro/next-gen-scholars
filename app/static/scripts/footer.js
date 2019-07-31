57bf5d14421fd7ea1fc33d5f135889373e4199dd
Bryant Lee <bl692@cornell.edu>
2019-07-31 10:24:49 -0700

    stylistic fixes

--- app/static/scripts/footer.js
+++ app/static/scripts/footer.js
@@ -1,16 +1,3 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     $("#footer_toggle").slideDown();
-//   } 
-//   else {
-//     $("#footer_toggle").slideUp();
-//   }
-//   prevScrollpos = currentScrollPos;
-
-// } 
-
 var isToggled = false;
 function toggleLanguage(){
   $('#footer_toggle').animate({'bottom': (isToggled ? '30px' : '70px')});

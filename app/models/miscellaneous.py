fa69c7c5df049b65b12ff854192ee530ef7d8a83
Bryant Lee <bl692@cornell.edu>
2019-08-06 11:17:21 -0700

    url now fixes itself if there is no http/https

--- app/models/miscellaneous.py
+++ app/models/miscellaneous.py
@@ -1,5 +1,5 @@
 from .. import db
-
+import re
 
 class EditableHTML(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +14,14 @@ class EditableHTML(db.Model):
         if editable_html_obj is None:
             editable_html_obj = EditableHTML(editor_name=editor_name, value='')
         return editable_html_obj
+
+
+# will fix URL in user forms so that they are clickable if http/https not included
+# you can always add http because it will get bumped up to https if available, 
+# but you can't bump down from https to http
+def fix_url(url):
+    match = re.search('^https?:\/\/', url)
+    if not match:
+        url = 'http://' + url
+    return url
+
